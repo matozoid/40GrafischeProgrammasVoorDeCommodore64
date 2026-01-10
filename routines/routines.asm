9955d5595d8b589f39315562de7930142551f87d
Danny van Bruggen <hexagonaal@gmail.com>
2026-01-10 16:50:58 +0100

    Some disassembly

--- routines/routines.asm
+++ routines/routines.asm
@@ -512,17 +512,21 @@ hires_plot_hr_0:              lda pixel_mask_1
                     sta PPORT
                     rts
                     
-hires_exit:         lda #>$0400
+hires_exit:
+                    ; reset screen pointer
+                    lda #>$0400
                     sta $0288
                     ; ???
                     lda CIA2PRDDRA
                     and #%11111100
                     sta CIA2PRDDRA
-                    ;
+                    ; disable bitmap mode
                     lda #%00011011
                     sta VMCTRL1
+                    ; disable multi-colour
                     lda #%11001000
                     sta VMCTRL2
+                    ; restore VIC to default setup
                     lda #%00010101
                     sta VMCSB
                     rts
